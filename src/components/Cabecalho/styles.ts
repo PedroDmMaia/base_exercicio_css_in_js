d8d459c8c3c0871c45e3df3fed790ef1b8acd051
Pedro Maia <pedrodelmonico@hotmail.com>
2024-01-15 16:59:30 -0300

    finalizando o projeto

---
+++ src/components/Cabecalho/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import variaveis from '../../styles/variaveis'
+// import { Props } from '.'
+
+const Cabecalho = styled.header`
+  background-color: ${variaveis.corSecundaria};
+  color: ${variaveis.corPrincipal};
+  text-align: center;
+  padding: 24px 0;
+`
+export default Cabecalho
